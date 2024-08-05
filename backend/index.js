8bab51984c341576331614e4fe10480f13363f83
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-05 10:54:09 +0530

    change front end link

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,11 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const router = require("./routes");
+const FRONTEND_URL = process.env.FRONTEND_URL
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: `${FRONTEND_URL}`,
   credentials: true
 }));
 app.use(express.json());
